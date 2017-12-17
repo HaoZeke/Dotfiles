14ff74bd395f9e843426b4620b51b8220ca7404b
HaoZeke <driger633@gmail.com>
2017-12-18 03:43:43 +0530

    Update calibre

---
+++ calibre/device_drivers_SMART_DEVICE_APP.py
@@ -0,0 +1,30 @@
+# settings for device drivers
+
+### Begin group: DEFAULT
+ 
+# format map
+# Ordered list of formats the device will accept
+format_map = cPickle.loads('\x80\x02]q\x01(U\x03lrfq\x02U\x03rarq\x03U\x03zipq\x04U\x03rtfq\x05U\x03litq\x06U\x03txtq\x07U\x04txtzq\x08U\x04textq\tU\x03htmq\nU\x04xhtmq\x0bU\x04htmlq\x0cU\x05htmlzq\rU\x05xhtmlq\x0eU\x03pdfq\x0fU\x03pdbq\x10U\x04updbq\x11U\x03pdrq\x12U\x03prcq\x13U\x04mobiq\x14U\x03azwq\x15U\x03docq\x16U\x04epubq\x17U\x03fb2q\x18U\x03djvq\x19U\x04djvuq\x1aU\x03lrxq\x1bU\x03cbrq\x1cU\x03cbzq\x1dU\x03cbcq\x1eU\x06oebzipq\x1fU\x02rbq U\x03impq!U\x03odtq"U\x03chmq#U\x03tpzq$U\x04azw1q%U\x03pmlq&U\x04pmlzq\'U\x03mbpq(U\x03tanq)U\x03snbq*U\x03xpsq+U\x04oxpsq,U\x04azw4q-U\x04bookq.U\x03zbfq/U\x04pobiq0U\x04docxq1U\x04docmq2U\x02mdq3U\x07textileq4U\x08markdownq5U\x05ibookq6U\x06ibooksq7U\x03ibaq8U\x04azw3q9U\x02psq:U\x05kepubq;U\x03kfxq<e.')
+ 
+# use subdirs
+# Place files in sub-directories if the device supports them
+use_subdirs = True
+ 
+# read metadata
+# Read metadata from files on device
+read_metadata = True
+ 
+# use author sort
+# Use author sort instead of author
+use_author_sort = False
+ 
+# save template
+# Template to control how books are saved
+save_template = u'{title} - {authors} ({id})'
+ 
+# extra customization
+# Extra customization
+extra_customization = cPickle.loads('\x80\x02]q\x01(\x89X\x00\x00\x00\x00X\x00\x00\x00\x00X\x00\x00\x00\x00\x89X\x04\x00\x00\x009090q\x02\x89X\x00\x00\x00\x00X\x00\x00\x00\x00X\x00\x00\x00\x00\x89X\x00\x00\x00\x00\x88X\x02\x00\x00\x0075q\x03\x88e.')
+ 
+
+
--- calibre/smtp.py
+++ calibre/smtp.py
@@ -3,10 +3,10 @@
 ### Begin group: DEFAULT
  
 # from 
-from_ = u'haozeke@gmx.com'
+from_ = u'archlibmailer@gmail.com'
  
 # accounts
-accounts = cPickle.loads('\x80\x02}q\x01X\x1a\x00\x00\x00amrita16thaug646@gmail.com]q\x02(U\nMOBI, EPUBq\x03\x89\x88es.')
+accounts = cPickle.loads('\x80\x02}q\x01(X\x1a\x00\x00\x00amrita16thaug646@gmail.com]q\x02(U\nMOBI, EPUBq\x03\x89\x88eX\x14\x00\x00\x00shahyadri@kindle.com]q\x04(U\x19AZW, MOBI, TPZ, PRC, AZW1q\x05\x89\x89eu.')
  
 # subjects
 subjects = cPickle.loads('\x80\x02}q\x01.')
@@ -14,17 +14,20 @@ subjects = cPickle.loads('\x80\x02}q\x01.')
 # aliases
 aliases = cPickle.loads('\x80\x02}q\x01.')
  
+# tags
+tags = cPickle.loads('\x80\x02}q\x01.')
+ 
 # relay host
-relay_host = u'mail.gmx.com'
+relay_host = u'smtp.gmail.com'
  
 # relay port
 relay_port = 465
  
 # relay username
-relay_username = u'haozeke@gmx.com'
+relay_username = u'archlibmailer@gmail.com'
  
 # relay password
-relay_password = '4449732d6665522d73746947492d486973637577'
+relay_password = '716973745e59646e41666b23364464667e36647a7132646d40624d797e687a43'
  
 # encryption
 encryption = 'SSL'
