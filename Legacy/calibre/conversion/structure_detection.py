8872121ded13c3f8fce96badc483811fe925a87f
HaoZeke <rohit.goswami@aol.com>
2018-05-07 04:48:42 +0530

    legacy: Delete

--- Legacy/calibre/conversion/structure_detection.py
+++
@@ -1,9 +0,0 @@
-{
-	'start_reading_at' : None,
-	'chapter_mark' : u'pagebreak',
-	'page_breaks_before' : u"//*[name()='h1' or name()='h2']",
-	'remove_first_image' : False,
-	'chapter' : u"//*[((name()='h1' or name()='h2') and re:test(., '\\s*((chapter|book|section|part)\\s+)|((prolog|prologue|epilogue)(\\s+|$))', 'i')) or @class = 'chapter']",
-	'remove_fake_margins' : True,
-	'insert_metadata' : True,
-}
