e660d04cb678d0335a303007c51f307368fe50bf
HaoZeke <rohit.goswami@aol.com>
2018-07-25 21:20:29 +0530

    doom: Fix pdfs by reverting modeline

--- dotfiles/emacs/.config/doom/init.el
+++ dotfiles/emacs/.config/doom/init.el
@@ -27,8 +27,8 @@
        :ui
       doom              ; what makes DOOM look the way it does
       doom-dashboard    ; a nifty splash screen for Emacs
-      (doom-modeline    ; a snazzy Atom-inspired mode-line
-       +new)            ; newer, possibly buggy implementation
+      doom-modeline    ; a snazzy Atom-inspired mode-line
+       ;; +new)            ; newer, possibly buggy implementation
       ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
       evil-goggles      ; display visual hints when editing in evil
       fci               ; a `fill-column' indicator
