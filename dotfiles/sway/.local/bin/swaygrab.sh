b3336fc1c7d59053137390c976fc800831233c6c
Rohit Goswami <rohit.goswami@aol.com>
2018-09-21 17:28:55 +0530

    sway: Update script

--- dotfiles/sway/.local/bin/swaygrab.sh
+++ dotfiles/sway/.local/bin/swaygrab.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Kanged from https://github.com/NicholasAsimov/dotfiles/blob/master/scripts/swaygrabselection
+# Updated by me (HaoZeke)
 
 set -e
 
@@ -9,10 +10,13 @@ filepath=$1
 if [ -z "$filepath" ]; then
     echo "Usage: $(basename "$0") filepath"
     echo "If you specify \"clipboard\" as the file path the image will be copied to primary clipboard."
-    exit 1
+    echo "Creating a time-stamped image"
+    slurp | grim -g - $(xdg-user-dir PICTURES)/Pictures/Screenshots/$(date +'%Y-%m-%d-%H%M%S_grim.png')
+    exit 0
 fi
 
 if [[ "$filepath" == "clipboard" ]]; then
+    echo "Copying image to the primary clipboard"
     slurp | grim -g - - | xclip -selection clipboard -t image/png -i
     exit 0
 fi
