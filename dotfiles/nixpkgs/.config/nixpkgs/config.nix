d8d26f64d291f97c0eb53ec566861f400a0c703e
Rohit Goswami <rgoswami@ieee.org>
2020-06-07 02:03:59 +0000

    conf: Refactor and add tidybayes.rethinking

--- dotfiles/nixpkgs/.config/nixpkgs/config.nix
+++ dotfiles/nixpkgs/.config/nixpkgs/config.nix
@@ -1,25 +1,39 @@
 {
   packageOverrides = super:
-    let self = super.pkgs;
+    let
+      self = super.pkgs;
+      rethinking = with self.rPackages;
+        buildRPackage {
+          name = "rethinking";
+          src = self.fetchFromGitHub {
+            owner = "rmcelreath";
+            repo = "rethinking";
+            rev = "d0978c7f8b6329b94efa2014658d750ae12b1fa2";
+            sha256 = "1qip6x3f6j9lmcmck6sjrj50a5azqfl6rfhp4fdj7ddabpb8n0z0";
+          };
+          propagatedBuildInputs = [ coda MASS mvtnorm loo shape rstan dagitty ];
+        };
+      tidybayes_rethinking = with self.rPackages;
+        buildRPackage {
+          name = "tidybayes.rethinking";
+          src = self.fetchFromGitHub {
+            owner = "mjskay";
+            repo = "tidybayes.rethinking";
+            rev = "df903c88f4f4320795a47c616eef24a690b433a4";
+            sha256 = "1jl3189zdddmwm07z1mk58hcahirqrwx211ms0i1rzbx5y4zak0c";
+          };
+          propagatedBuildInputs =
+            [ dplyr tibble rlang MASS tidybayes rethinking rstan ];
+        };
     in {
-
       rEnv = super.rWrapper.override {
         packages = with self.rPackages; [
           ggplot2
           knitr
           tidyverse
           tidybayes
-          (buildRPackage {
-            name = "rethinking";
-            src = self.fetchFromGitHub {
-              owner = "rmcelreath";
-              repo = "rethinking";
-              rev = "d0978c7f8b6329b94efa2014658d750ae12b1fa2";
-              sha256 = "1qip6x3f6j9lmcmck6sjrj50a5azqfl6rfhp4fdj7ddabpb8n0z0";
-            };
-            propagatedBuildInputs =
-              [ coda MASS mvtnorm loo shape rstan dagitty ];
-          })
+          rethinking
+          tidybayes_rethinking
         ];
       };
     };
