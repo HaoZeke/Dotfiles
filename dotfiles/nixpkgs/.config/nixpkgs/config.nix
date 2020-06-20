5f2ae2764431fa731ec846904204c6ae676fb173
Rohit Goswami <rgoswami@ieee.org>
2020-06-20 15:28:16 +0000

    nix: Update rev

--- dotfiles/nixpkgs/.config/nixpkgs/config.nix
+++ dotfiles/nixpkgs/.config/nixpkgs/config.nix
@@ -8,7 +8,7 @@
           src = self.fetchFromGitHub {
             owner = "rmcelreath";
             repo = "rethinking";
-            rev = "d0978c7f8b6329b94efa2014658d750ae12b1fa2";
+            rev = "ed5759876b2c2123f16425ea715a70fec2dfafaf";
             sha256 = "1qip6x3f6j9lmcmck6sjrj50a5azqfl6rfhp4fdj7ddabpb8n0z0";
           };
           propagatedBuildInputs = [ coda MASS mvtnorm loo shape rstan dagitty ];
