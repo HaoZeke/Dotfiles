5331decb712709bc8c400b9bb64e6976a5d5bdb0
Rohit Goswami <rgoswami@ieee.org>
2020-08-08 02:37:43 +0000

    hpc: Clean and move lmod files

--- dotfiles/hpc/Modulefiles/autotools/autotools.lua
+++
@@ -1,9 +0,0 @@
-help([[
-The autotools modulefile defines m4, autoconf, automake and libtool
-]])
--- 1 --
-if (os.getenv("USER") ~= "root") then
-append_path("PATH", ".")
-end
--- 2 --
-load("autotools/m4/1.4.14","autotools/autoconf/2.64","autotools/automake/1.11.1","autotools/libtool/2.4")
