381a62213375a2f63b1e784d041cbc1362b1bbd3
Rohit Goswami <rgoswami@ieee.org>
2020-11-01 00:50:02 +0000

    convRmd: Refactor and rename

--- dotfiles/common/.local/bin/fileHelpers/getHTML.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-read -p 'RMD File: ' rmdfile
-Rscript -e "library(rmarkdown); render('$rmdfile.Rmd',c('html_document'));"
-mv $rmdfile.html tmp.html
-mjpage --output MML <tmp.html >$rmdfile.html
-rm -rf tmp.html
