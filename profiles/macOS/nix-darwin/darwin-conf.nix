aab5fadb6ee0026d711d1afa2be5fe8e5d045548
Rohit Goswami <rog32@hi.is>
2021-04-12 08:45:41 +0000

    nixd: Clean config

--- profiles/macOS/nix-darwin/darwin-conf.nix
+++ profiles/macOS/nix-darwin/darwin-conf.nix
@@ -111,25 +111,27 @@ in {
   };
 
   # Spacebar #
-  services.spacebar.enable = true;
-  services.spacebar.package = pkgs.spacebar;
-  services.spacebar.config = {
-    debug_output = "on";
-    position = "top";
-    clock_format = "%R";
-    space_icon_strip = "   ";
-    text_font = ''"CaskaydiaCove Nerd Font:Regular:12.0"'';
-    icon_font = ''"CaskaydiaCove Nerd Font:Regular:12.0"'';
-    background_color = "0xff2e3440";
-    foreground_color = "0xffd8dee9";
-    space_icon_color = "0xff81a1c1";
-    dnd_icon_color = "0xff81a1c1";
-    clock_icon_color = "0xff81a1c1";
-    power_icon_color = "0xff81a1c1";
-    battery_icon_color = "0xff81a1c1";
-    power_icon_strip = " ";
-    space_icon = "";
-    clock_icon = "";
-    dnd_icon = "";
+  services.spacebar = {
+    enable = true;
+    package = pkgs.spacebar;
+    config = {
+      debug_output = "on";
+      position = "top";
+      clock_format = "%R";
+      space_icon_strip = "   ";
+      text_font = ''"CaskaydiaCove Nerd Font:Regular:12.0"'';
+      icon_font = ''"CaskaydiaCove Nerd Font:Regular:12.0"'';
+      background_color = "0xff2e3440";
+      foreground_color = "0xffd8dee9";
+      space_icon_color = "0xff81a1c1";
+      dnd_icon_color = "0xff81a1c1";
+      clock_icon_color = "0xff81a1c1";
+      power_icon_color = "0xff81a1c1";
+      battery_icon_color = "0xff81a1c1";
+      power_icon_strip = " ";
+      space_icon = "";
+      clock_icon = "";
+      dnd_icon = "";
+    };
   };
 }
