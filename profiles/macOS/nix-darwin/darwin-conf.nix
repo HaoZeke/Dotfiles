7fc979f36002e02f5ee1b817711b2722f3609e2f
Rohit Goswami <rog32@hi.is>
2021-04-12 05:10:07 +0000

    nixd: Update packages and also manage nix

--- profiles/macOS/nix-darwin/darwin-conf.nix
+++ profiles/macOS/nix-darwin/darwin-conf.nix
@@ -3,8 +3,7 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    with pkgs; [ 
+  environment.systemPackages = with pkgs; [
     neovim
     # GUI
     emacs
@@ -18,9 +17,14 @@
     tmux
     htop
     cmake
+    wakatime
     # Managers
     rustup
-    ];
+    # Nix
+    nixfmt
+    nox
+    lorri
+  ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -28,10 +32,10 @@
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
