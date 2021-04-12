f037bac6d6f1bde8216cab0d15ef13f54859ccb8
Rohit Goswami <rog32@hi.is>
2021-04-12 04:06:40 +0000

    nixd: Init macos nix-darwin conf

---
+++ profiles/macOS/nix-darwin/darwin-conf.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    with pkgs; [ 
+    neovim
+    # GUI
+    emacs
+    alacritty
+    # Base Toolset
+    git
+    ripgrep
+    fd
+    zsh
+    tmux
+    htop
+    cmake
+    # Managers
+    rustup
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+  # Manage users better
+  users.nix.configureBuildUsers = true;
+
+}
