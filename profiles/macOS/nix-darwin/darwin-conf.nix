b0ee18ef8a36959f86ec3314819884fd204676ed
Rohit Goswami <rog32@hi.is>
2021-04-14 00:32:09 +0000

    nix: Use unstable and add flakes

--- profiles/macOS/nix-darwin/darwin-conf.nix
+++ profiles/macOS/nix-darwin/darwin-conf.nix
@@ -53,7 +53,10 @@ in {
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
+  nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
