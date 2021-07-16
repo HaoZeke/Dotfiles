da8799ede93e40780e8c05c4bcedf48ce5a1e0fc
Rohit Goswami <rog32@hi.is>
2021-07-16 14:08:37 +0000

    bash: Add an activation script

---
+++ profiles/common/shell/bash/99_activate_bin.sh
@@ -0,0 +1,19 @@
+######################
+# Activation Scripts #
+######################
+
+# Rust
+#######
+
+# Almost all of these are with cargo install blah blah
+
+# TODO: Update to test that these exist
+eval "$(zoxide init bash)"
+eval "$(starship init bash)"
+
+# Python
+########
+
+# Almost never use this, but still
+
+eval "$(micromamba shell hook -s bash)"
