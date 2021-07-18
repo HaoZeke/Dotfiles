69b19d68986cdcd39232a0d56971c319e4232fb5
Rohit Goswami <rog32@hi.is>
2021-07-18 23:28:33 +0000

    bombadil: Refactor to conditionally add mamba

---
+++ profiles/common/shell/bash/01_mamba.bash
@@ -0,0 +1,20 @@
+export USE_MAMBA="__[use_mamba]__"
+
+if [ "$USE_MAMBA" == "yes" ]; then
+    # mamba initialize >>>
+    # !! Contents within this block are managed by 'mamba init' !!
+    export MAMBA_EXE="$HOME/.local/bin/micromamba";
+    export MAMBA_ROOT_PREFIX="$HOME/.micromamba";
+    __mamba_setup="$('$HOME/.local/bin/micromamba' shell hook --shell bash --prefix '$HOME/.micromamba' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__mamba_setup"
+    else
+    if [ -f "$HOME/.micromamba/etc/profile.d/mamba.sh" ]; then
+        . "$HOME/.micromamba/etc/profile.d/mamba.sh"
+    else
+        export PATH="$HOME/.micromamba/bin:$PATH"
+    fi
+    fi
+    unset __mamba_setup
+    # <<< mamba initialize <<<
+fi
