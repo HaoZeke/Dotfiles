fcaf0df57e9dd35f8e9c37228089f7366f7c65c7
Rohit Goswami <rog32@hi.is>
2021-07-19 23:55:20 +0000

    nvm: Init variable and conf

---
+++ profiles/common/shell/bash/02_nvm.bash
@@ -0,0 +1,5 @@
+if [ "$USE_NVM" == "yes" || "$USE_NVM" == "true" ]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
