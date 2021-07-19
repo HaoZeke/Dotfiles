22f6d27973e0785f9a03f842b7ef131122c099ee
Rohit Goswami <rog32@hi.is>
2021-07-19 22:51:31 +0000

    vars: Refactor usage

---
+++ profiles/common/shell/posix/00_bombadil_vars.sh
@@ -0,0 +1,7 @@
+# Conditionals
+###############
+
+# These are set in various bombadil var files
+# Typically true or yes is the condition
+export USE_MAMBA="__[use_mamba]__"
+export IS_REMOTE="__[is_remote]__"
