b4d3aa3875846c505974f5308fa6294b177f1398
Rohit Goswami <rog32@hi.is>
2021-07-20 00:58:41 +0000

    bombadil: Init nvm

--- profiles/common/shell/posix/00_bombadil_vars.sh
+++ profiles/common/shell/posix/00_bombadil_vars.sh
@@ -4,4 +4,5 @@
 # These are set in various bombadil var files
 # Typically true or yes is the condition
 export USE_MAMBA="__[use_mamba]__"
+export USE_NVM="__[use_nvm]__"
 export IS_REMOTE="__[is_remote]__"
