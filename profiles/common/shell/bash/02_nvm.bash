7f1441c61634c047af46ec7f892f294a5c9a9ad8
Rohit Goswami <rog32@hi.is>
2021-07-20 00:40:06 +0000

    shell: Use bashisms and posixisms

--- profiles/common/shell/bash/02_nvm.bash
+++ profiles/common/shell/bash/02_nvm.bash
@@ -1,4 +1,6 @@
-if [ "$USE_NVM" == "yes" || "$USE_NVM" == "true" ]; then
+#!/usr/bin/env bash
+
+if [[ "$USE_NVM" == "yes" || "$USE_NVM" == "true" ]]; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
