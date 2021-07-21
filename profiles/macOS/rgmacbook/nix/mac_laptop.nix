eda9d4bb520276775952e683c9b4749307bba2dc
Rohit Goswami <rog32@hi.is>
2021-07-21 01:51:10 +0000

    nix: Update hashes

--- profiles/macOS/rgmacbook/nix/mac_laptop.nix
+++ profiles/macOS/rgmacbook/nix/mac_laptop.nix
@@ -5,14 +5,16 @@
   # NUR setup
   # https://github.com/nix-community/NUR
   packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball {
-      # Get the revision by choosing a version from https://github.com/nix-community/NUR/commits/master
-      url = "https://github.com/nix-community/NUR/archive/3a6a6f4da737da41e27922ce2cfacf68a109ebce.tar.gz";
-      # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
-      sha256 = "04387gzgl8y555b3lkz9aiw9xsldfg4zmzp930m62qw8zbrvrshd";
-    }
-    ) {
-      inherit pkgs;
-    };
+    nur = import
+      (builtins.fetchTarball {
+        # Get the revision by choosing a version from https://github.com/nix-community/NUR/commits/master
+        url = "https://github.com/nix-community/NUR/archive/4a81d63c672ffdbbb999eb2549c0eb0934b3384b.tar.gz";
+        # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
+        sha256 = "09bg0xifdk6rw35w472rk0dj69w08c1ksb3a3qrzay0ww69495zk";
+      }
+      )
+      {
+        inherit pkgs;
+      };
   };
 }
