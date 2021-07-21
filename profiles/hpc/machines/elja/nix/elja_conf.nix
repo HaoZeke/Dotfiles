8bea3b0f91ab2af1ce4f42b62d26601dadc0c375
Rohit Goswami <rog32@hi.is>
2021-07-21 00:45:13 +0000

    elja: Update channel form

--- profiles/hpc/machines/elja/nix/elja_conf.nix
+++ profiles/hpc/machines/elja/nix/elja_conf.nix
@@ -1,10 +1,13 @@
+# Uses the channel form of the installation
+# https://github.com/nix-community/NUR/issues/150
 {
   # For Intel stuff
   allowUnfree = true;
   # NUR setup
   # https://github.com/nix-community/NUR
+  pkgs = import <unstable>; # nixos-unstable channel
   packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    nur = import <nur> { # Also defined as a channel
       inherit pkgs;
     };
   };
