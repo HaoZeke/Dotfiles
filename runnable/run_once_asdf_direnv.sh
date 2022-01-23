522f3babac76452e62ac371e53155215df1b2795
Rohit Goswami <rog32@hi.is>
2022-01-23 11:16:04 +0000

    ENH: Add direnv, asdf helper

---
+++ runnable/run_once_asdf_direnv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Although I no longer recommend asdf, (shims are awful)
+# This is the fastest way to get direnv up and running
+
+if [[ ! -d ~/.asdf ]]; then
+    mkdir ~/.asdf
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+fi
+
+export ASDF_CONFIG_FILE="~/.config/asdf/asdfrc"
+
+asdf plugin-add direnv
+asdf install direnv latest
+asdf global direnv latest
