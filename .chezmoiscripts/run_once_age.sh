a7c0d0d49a418bc7f7ffc6f2127d0093c88f04d2
Rohit Goswami <rog32@hi.is>
2022-03-31 17:23:31 +0000

    SHELL: Add an age binary

---
+++ .chezmoiscripts/run_once_age.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This is problematic, and necessary
+# Will try, in order:
+# - brew
+# - source build (with go)
+# - prebuilt binaries
+
+if command -v age >/dev/null 2>&1; then
+    echo "got age, good to go"
+    # has age, continue
+else
+    if command -v brew >/dev/null 2>&1; then
+        brew install age # Typically MacOS
+    fi
+    if command -v go >/dev/null 2>&1; then
+        git clone https://filippo.io/age && cd age
+        go build -o . filippo.io/age/cmd/...
+        mv age age-keygen ~/.local/bin
+        export PATH=$PATH:$HOME/.local/bin
+    fi
+fi
