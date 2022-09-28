69066bc5733d83d7f570b71f74bd8c91256adbd6
Rohit Goswami <rog32@hi.is>
2022-09-28 15:24:29 +0000

    MAINT: Move I/O for power10k zsh

--- dot_config/shellrc/posix.d/04_sources.sh
+++ dot_config/shellrc/posix.d/04_sources.sh
@@ -1,16 +1,6 @@
 # Sources
 ############
 
-# GRE Word List
-if [[ $- = *i* ]]; then
-	if [[ -d $HOME/.local/greWords/gre-cli-words ]]; then
-		$HOME/.local/greWords/gre-cli-words/random_gre.sh $HOME/.local/greWords/gre-cli-words/custom_gre_word_list
-		echo "\n"
-	else
-		echo "You don't have gre-cli-words setup, try running getGREwords"
-	fi
-fi
-
 # Nix
 if [[ $- = *i* ]]; then
 	if [[ -f /etc/profile.d/nix.sh ]]; then
