23be04a2be5082901a4edd99113562a6f61c3059
Rohit Goswami <rog32@hi.is>
2022-11-06 15:51:41 +0000

    ENH: Don't activate on ZSH

--- dot_config/shellrc/bash.d/99_activate_bin.bash
+++ dot_config/shellrc/bash.d/99_activate_bin.bash
@@ -8,6 +8,8 @@
 # Almost all of these are with cargo install blah blah
 
 # TODO: Update to test that these exist
-eval "$(zoxide init bash)"
-eval "$(starship init bash)"
-eval "$(mcfly init bash)"
+if $BASH; then
+    eval "$(zoxide init bash)"
+    eval "$(starship init bash)"
+    eval "$(mcfly init bash)"
+fi
