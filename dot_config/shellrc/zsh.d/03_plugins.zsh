25c30476e0a9e9642113129b23594ede1bf7af46
Rohit Goswami <rog32@hi.is>
2022-11-06 15:59:55 +0000

    MAINT: Switch to zi

--- dot_config/shellrc/zsh.d/03_plugins.zsh
+++ dot_config/shellrc/zsh.d/03_plugins.zsh
@@ -1,12 +1,19 @@
 # Plugin Management
 #####################
 
-if [[ ! -d ~/.zinit ]]; then
-    mkdir ~/.zinit
-    git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin
+if [[ ! -d ~/.zi ]]; then
+    typeset -Ag ZI
+    typeset -gx ZI[HOME_DIR]="${HOME}/.zi" ZI[BIN_DIR]="${ZI[HOME_DIR]}/bin"
+    mkdir -p "$ZI[BIN_DIR]"
+
+    compaudit | xargs chown -R "$(whoami)" "$ZI[HOME_DIR]"
+    compaudit | xargs chmod -R go-w "$ZI[HOME_DIR]"
+    command git clone https://github.com/z-shell/zi.git "$ZI[BIN_DIR]"
 fi
 
-source ~/.zinit/bin/zinit.zsh
+typeset -A ZI
+ZI[BIN_DIR]="${HOME}/.zi/bin"
+source "${ZI[BIN_DIR]}/zi.zsh"
 
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
+autoload -Uz _zi
+(( ${+_comps} )) && _comps[zi]=_zi
