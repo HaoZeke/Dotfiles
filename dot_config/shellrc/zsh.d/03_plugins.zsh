0fb937581e7016cae50c445536023329365ac992
Rohit Goswami <rog32@hi.is>
2023-08-02 21:09:26 +0000

    MAINT: Replace zi for zinit from zdharma-continuum

--- dot_config/shellrc/zsh.d/03_plugins.zsh
+++ dot_config/shellrc/zsh.d/03_plugins.zsh
@@ -1,19 +1,10 @@
 # Plugin Management
 #####################
 
-if [[ ! -d ~/.zi ]]; then
-    typeset -Ag ZI
-    typeset -gx ZI[HOME_DIR]="${HOME}/.zi" ZI[BIN_DIR]="${ZI[HOME_DIR]}/bin"
-    mkdir -p "$ZI[BIN_DIR]"
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
 
-    compaudit | xargs chown -R "$(whoami)" "$ZI[HOME_DIR]"
-    compaudit | xargs chmod -R go-w "$ZI[HOME_DIR]"
-    command git clone https://github.com/z-shell/zi.git "$ZI[BIN_DIR]"
-fi
-
-typeset -A ZI
-ZI[BIN_DIR]="${HOME}/.zi/bin"
-source "${ZI[BIN_DIR]}/zi.zsh"
-
-autoload -Uz _zi
-(( ${+_comps} )) && _comps[zi]=_zi
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
