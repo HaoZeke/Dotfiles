425546301a38563d068e5ccbb7f01f4d56b884bf
Rohit Goswami <rog32@hi.is>
2023-08-19 13:13:56 +0000

    ENH: Add a pulsating dialog

--- dot_local/bin/executable_calibre_and_sync.sh
+++ dot_local/bin/executable_calibre_and_sync.sh
@@ -5,17 +5,12 @@
 calibre
 
 # Setup and run sync
+zenity --notification --text="Start syncing"
 RCLONE_PASSWORD_COMMAND='secret-tool lookup service "rclone" user "calisync"'
-
 (
-  rclone sync -P ~/CalibreLibs/Synced pCloud:/Calibre --ask-password=false 2>&1 | \
+  rclone sync -P ~/CalibreLibs/Synced/Study pCloud:/Calibre/Study --ask-password=false \
+      --password-command="$RCLONE_PASSWORD_COMMAND" 2>&1 | \
   while read -r line; do
-    # Check if the line contains the "Checks" status
-    if [[ $line == *"Checks:"* ]]; then
-      # Extract the percentage from the "Checks" line
-      percent=$(echo $line | awk -F ',' '{print $2}' | grep -oP '\s+\K[0-9]+(?=%)')
-      echo $percent
-    fi
     echo "# $line"
   done
 ) |
@@ -23,5 +18,7 @@ zenity --progress \
   --title="Syncing Calibre Library with pCloud" \
   --text="Syncing..." \
   --percentage=0 \
+  --pulsate \
   --auto-close \
   --auto-kill
+zenity --notification --text="Sync completed"
