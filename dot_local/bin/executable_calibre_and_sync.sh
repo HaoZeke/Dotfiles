6effaf7887fd02cbd8c01a52a1a2197309b03ee3
Rohit Goswami <rog32@hi.is>
2023-08-19 12:28:38 +0000

    ENH: Stop waiting, add zenity progress

--- dot_local/bin/executable_calibre_and_sync.sh
+++ dot_local/bin/executable_calibre_and_sync.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+# $HOME/.local/bin/calibre_and_sync.sh
 
-# Start calibre and wait
+# Start Calibre and wait
 calibre
-wait $!
 
 # Setup and run sync
 RCLONE_PASSWORD_COMMAND='secret-tool lookup service "rclone" user "calisync"'
-rclone sync -P ~/CalibreLibs/Synced pCloud:/Calibre --ask-password=false
+
+(
+  rclone sync -P ~/CalibreLibs/Synced pCloud:/Calibre --ask-password=false 2>&1 | \
+  while read -r line; do
+    # Check if the line contains the "Checks" status
+    if [[ $line == *"Checks:"* ]]; then
+      # Extract the percentage from the "Checks" line
+      percent=$(echo $line | awk -F ',' '{print $2}' | grep -oP '\s+\K[0-9]+(?=%)')
+      echo $percent
+    fi
+    echo "# $line"
+  done
+) |
+zenity --progress \
+  --title="Syncing Calibre Library with pCloud" \
+  --text="Syncing..." \
+  --percentage=0 \
+  --auto-close \
+  --auto-kill
