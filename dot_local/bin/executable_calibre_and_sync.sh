86d9f71aa8d0ec149709a2941c77c69865de9a71
Rohit Goswami <rog32@hi.is>
2023-08-19 13:14:11 +0000

    ENH: Just spawn for rclone

--- dot_local/bin/executable_calibre_and_sync.sh
+++ dot_local/bin/executable_calibre_and_sync.sh
@@ -7,18 +7,5 @@ calibre
 # Setup and run sync
 zenity --notification --text="Start syncing"
 RCLONE_PASSWORD_COMMAND='secret-tool lookup service "rclone" user "calisync"'
-(
-  rclone sync -P ~/CalibreLibs/Synced/Study pCloud:/Calibre/Study --ask-password=false \
-      --password-command="$RCLONE_PASSWORD_COMMAND" 2>&1 | \
-  while read -r line; do
-    echo "# $line"
-  done
-) |
-zenity --progress \
-  --title="Syncing Calibre Library with pCloud" \
-  --text="Syncing..." \
-  --percentage=0 \
-  --pulsate \
-  --auto-close \
-  --auto-kill
+foot -- sh -c "rclone sync -P ~/CalibreLibs/Synced pCloud:/Calibre --ask-password=false --password-command=\"$RCLONE_PASSWORD_COMMAND\""
 zenity --notification --text="Sync completed"
