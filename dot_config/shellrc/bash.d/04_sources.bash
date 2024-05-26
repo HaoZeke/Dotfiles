cdea576b352a0ea10605f11815e23aecbbd96ca5
Rohit Goswami <rog32@hi.is>
2024-05-26 15:37:56 +0000

    MAINT: Move posix->bash sources
    Since it wasn't posix compliant anyway

---
+++ dot_config/shellrc/bash.d/04_sources.bash
@@ -0,0 +1,15 @@
+# Sources
+############
+
+# Nix
+if [[ $- = *i* ]]; then
+    if [[ -f /etc/profile.d/nix.sh ]]; then
+        . /etc/profile.d/nix.sh
+    elif [[ -d /nix ]]; then
+        if [[ ! $(uname)=="Darwin" ]]; then
+            source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+        fi
+    else
+        echo "You don't have nix installed"
+    fi
+fi
