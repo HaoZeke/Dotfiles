3e2304704815ea7973d2683c52f08ac0d9f30a9f
Rohit Goswami <rog32@hi.is>
2025-01-30 01:49:32 +0000

    MAINT: Check if helpers exist

--- dot_config/shellrc/bash.d/99_activate_bin.bash
+++ dot_config/shellrc/bash.d/99_activate_bin.bash
@@ -7,9 +7,22 @@
 
 # Almost all of these are with cargo install blah blah
 
-# TODO: Update to test that these exist
 if $BASH; then
-    eval "$(zoxide init bash)"
-    eval "$(starship init bash)"
-    eval "$(mcfly init bash)"
+    if command -v zoxide &> /dev/null; then
+        eval "$(zoxide init bash)"
+    else
+        echo "zoxide not found. Install it with 'cargo install zoxide' or your package manager."
+    fi
+
+    if command -v starship &> /dev/null; then
+        eval "$(starship init bash)"
+    else
+        echo "starship not found. Install it with 'cargo install starship' or your package manager."
+    fi
+
+    if command -v mcfly &> /dev/null; then
+        eval "$(mcfly init bash)"
+    else
+        echo "mcfly not found. Install it with 'cargo install mcfly' or your package manager."
+    fi
 fi
