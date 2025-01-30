f0971a1fdbd770f02cea3e1040dc6ec5be35f102
Rohit Goswami <rog32@hi.is>
2025-01-30 02:38:25 +0000

    MAINT: Restructure for nix

--- dot_config/shellrc/bash.d/03_locale_nix.bash
+++ dot_config/shellrc/bash.d/03_locale_nix.bash
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+{{ if eq .setup_nix "yes" }}
+
 if [[ "$HAS_NIX" == "yes" || "$HAS_NIX" == "true" ]]; then
     export LOCALE_ARCHIVE="$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive"
 fi
+
+# Nix
+if [[ $- = *i* ]]; then
+    if [[ -f /etc/profile.d/nix.sh ]]; then
+        . /etc/profile.d/nix.sh
+    elif [[ -d /nix ]]; then
+        if [[ ! $(uname)=="Darwin" ]]; then
+            source "$HOME/.nix-profile/etc/profile.d/nix.sh"
+        fi
+    else
+        if [[ -v DEBUG_RGDOTS ]]; then
+            echo "You don't have nix installed"
+        fi
+    fi
+fi
+
+{{ end }}
