e2d5081da4c4790c3d1144b56260bb6caacdf06b
Rohit Goswami <rog32@hi.is>
2025-01-30 03:49:23 +0000

    MAINT: Fix more typos

--- dot_config/shellrc/posix.d/00_show_gre.sh
+++ dot_config/shellrc/posix.d/00_show_gre.sh
@@ -2,7 +2,6 @@
 if [[ $- = *i* ]]; then
     if [[ -d $HOME/.local/greWords/gre-cli-words ]]; then
         $HOME/.local/greWords/gre-cli-words/random_gre.sh $HOME/.local/greWords/gre-cli-words/custom_gre_word_list
-        echo "\n"
     else
         echo "You don't have gre-cli-words setup, try running getGREwords"
     fi
