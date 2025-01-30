730b3ff79cf46c5392b2d8cd049d0607e6888c4d
Rohit Goswami <rog32@hi.is>
2025-01-30 03:45:13 +0000

    MAINT: Less chances of recursion

--- dot_config/shellrc/bash.d/99_activate_bin.bash
+++ dot_config/shellrc/bash.d/99_activate_bin.bash
@@ -7,22 +7,14 @@
 
 # Almost all of these are with cargo install blah blah
 
-if $BASH; then
-    if command -v zoxide &> /dev/null; then
-        eval "$(zoxide init bash)"
-    else
-        echo "zoxide not found. Install it with 'cargo install zoxide' or your package manager."
-    fi
+if command -v zoxide &> /dev/null; then
+    eval "$(zoxide init bash)"
+fi
 
-    if command -v starship &> /dev/null; then
-        eval "$(starship init bash)"
-    else
-        echo "starship not found. Install it with 'cargo install starship' or your package manager."
-    fi
+if command -v starship &> /dev/null; then
+    eval "$(starship init bash)"
+fi
 
-    if command -v mcfly &> /dev/null; then
-        eval "$(mcfly init bash)"
-    else
-        echo "mcfly not found. Install it with 'cargo install mcfly' or your package manager."
-    fi
+if command -v mcfly &> /dev/null; then
+    eval "$(mcfly init bash)"
 fi
