4fa95cd8589d934237cf12b83de6936f907a24e7
Rohit Goswami <rog32@hi.is>
2025-08-30 17:52:46 +0200

    ENH: Add a nicer fish greeting

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,10 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    function fish_greeting
+        if test -d "$HOME/.local/greWords/gre-cli-words"
+            "$HOME/.local/greWords/gre-cli-words/random_gre.sh" "$HOME/.local/greWords/gre-cli-words/custom_gre_word_list"
+        else
+            echo "You don't have gre-cli-words setup, try running getGREwords"
+        end
+    end
 end
