bac5001970841cfab1281fcf8c9bfb1e3e8b9bcc
Rohit Goswami <rog32@hi.is>
2025-08-30 23:58:04 +0200

    MAINT: Minor restructure

--- dot_config/shellrc/bash.d/99_activate_bin.bash
+++ dot_config/shellrc/bash.d/99_activate_bin.bash
@@ -5,16 +5,6 @@
 # Rust
 #######
 
-# Almost all of these are with cargo install blah blah
-
-programs=(zoxide starship mcfly)
-
-for prog in "${programs[@]}"; do
-    if command -v "$prog" &>/dev/null; then
-        eval "$("$prog" init bash)"
-    fi
-done
-
 # https://www.x-cmd.com/
 [ ! -f "$HOME/.x-cmd.root/X" ] || . "$HOME/.x-cmd.root/X"
 
@@ -23,6 +13,15 @@ BLE_SH_PATH="$HOME/.local/share/blesh/ble.sh"
 if [ -f "$BLE_SH_PATH" ]; then
   source "$BLE_SH_PATH"
 fi
+
+# Almost all of these are with cargo install blah blah
+programs=(zoxide starship mcfly)
+
+for prog in "${programs[@]}"; do
+    if command -v "$prog" &>/dev/null; then
+        eval "$("$prog" init bash)"
+    fi
+done
 
 # Emacs Stuff (cross platform)
 # Local Variables:
