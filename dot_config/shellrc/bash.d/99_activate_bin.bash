5f5d56948a4d3b765acb943f2101655ca7b9aecc
Rohit Goswami <rog32@hi.is>
2025-08-31 00:14:54 +0200

    MAINT: Restructure for ble.sh

--- dot_config/shellrc/bash.d/99_activate_bin.bash
+++ dot_config/shellrc/bash.d/99_activate_bin.bash
@@ -8,12 +8,6 @@
 # https://www.x-cmd.com/
 [ ! -f "$HOME/.x-cmd.root/X" ] || . "$HOME/.x-cmd.root/X"
 
-# ble.sh isn't part of x-cmd
-BLE_SH_PATH="$HOME/.local/share/blesh/ble.sh"
-if [ -f "$BLE_SH_PATH" ]; then
-  source "$BLE_SH_PATH"
-fi
-
 # Almost all of these are with cargo install blah blah
 programs=(zoxide starship mcfly)
 
