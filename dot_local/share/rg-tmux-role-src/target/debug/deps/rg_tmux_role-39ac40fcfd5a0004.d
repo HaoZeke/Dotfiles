af1f95dac76b10a0546d40ffa3f53f69074ecfdd
Rohit Goswami <rgoswami@ieee.org>
2026-04-22 19:36:50 +0200

    fix(tmux): drop tracked build artifacts

--- dot_local/share/rg-tmux-role-src/target/debug/deps/rg_tmux_role-39ac40fcfd5a0004.d
+++
@@ -1,5 +0,0 @@
-/home/rgoswami/.local/share/chezmoi/dot_local/share/rg-tmux-role-src/target/debug/deps/rg_tmux_role-39ac40fcfd5a0004.d: main.rs
-
-/home/rgoswami/.local/share/chezmoi/dot_local/share/rg-tmux-role-src/target/debug/deps/rg_tmux_role-39ac40fcfd5a0004: main.rs
-
-main.rs:
